2469ca678992f4fdb1fad7232ede63c19468f299
T0ast <minhthinle69@gmail.com>
2026-02-10 13:58:18 +0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+  };
+
+  outputs = inputs @ { self, nixpkgs, home-manager, ... }: {
+
+    nixosConfigurations.t0ast-nix = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = inputs;
+      modules = [
+        ./configuration.nix
+	home-manager.nixosModules.home-manager
+	{
+	  home-manager.extraSpecialArgs = {inherit inputs self;};
+          home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPackages = true;
+	  home-manager.users.t0ast = import ./home.nix;
+	}
+      ];
+    };
+
+  };
+}
