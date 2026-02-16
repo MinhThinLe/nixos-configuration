1f1678f5512da9935f8bb1891d6121702a9b20e2
T0ast <minhthinle69@gmail.com>
2026-02-16 17:37:02 +0700

    Fix: autologin not working

--- homeModules/legacy-dotfiles.nix
+++ homeModules/legacy-dotfiles.nix
@@ -22,7 +22,7 @@
 
     home.file.".zprofile".text = ''
     if [ -z "$WAYLAND_DISPLAY" ] && [ -n "$XDG_VTNR" ] && [ "$XDG_VTNR" -eq 1 ] ; then
-        exec niri-session
+        exec niri-session -l
     fi
     '';
 }
