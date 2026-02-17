3018ba8bffa2b30def575d540b3778ad24b801be
T0ast <minhthinle69@gmail.com>
2026-02-17 21:20:14 +0700

    Desktop entry for zen browser uni profile

--- home.nix
+++ home.nix
@@ -1,70 +1,87 @@
 {
-  inputs,
-  config,
-  pkgs,
-  ...
+    inputs,
+    config,
+    pkgs,
+    ...
 }:
 
 {
-  home.username = "t0ast";
-  home.homeDirectory = "/home/t0ast";
+    home.username = "t0ast";
+    home.homeDirectory = "/home/t0ast";
 
-  imports = [
-    inputs.zen-browser.homeModules.twilight-official
-    ./homeModules/zen-browser.nix
-    ./homeModules/legacy-dotfiles.nix
-    ./homeModules/foot.nix
-  ];
+    imports = [
+        inputs.zen-browser.homeModules.twilight-official
+        ./homeModules/zen-browser.nix
+        ./homeModules/legacy-dotfiles.nix
+        ./homeModules/foot.nix
+    ];
 
-  home.packages = with pkgs; [
-    bat
-    bluetui
-    btop
-    eza
-    fastfetch
-    fluent-icon-theme
-    git
-    hypridle
-    hyprlock
-    kanshi
-    mako
-    nemo
-    nemo-fileroller
-    networkmanager_dmenu
-    pwvucontrol
-    ripgrep
-    rofi
-    rofi-calc
-    starship
-    swaybg
-    ungoogled-chromium
-    waybar
-    wl-mirror
-    zathura
-    zoxide
-  ];
+    home.packages = with pkgs; [
+        bat
+        bluetui
+        btop
+        eza
+        fastfetch
+        fluent-icon-theme
+        git
+        hypridle
+        hyprlock
+        kanshi
+        mako
+        nemo
+        nemo-fileroller
+        networkmanager_dmenu
+        pwvucontrol
+        ripgrep
+        rofi
+        rofi-calc
+        starship
+        swaybg
+        ungoogled-chromium
+        waybar
+        wl-mirror
+        zathura
+        zoxide
+    ];
 
-  programs.git = {
-    enable = true;
-    settings = {
-      user = {
-        email = "minhthinle69@gmail.com";
-        name = "T0ast";
-      };
+    programs.git = {
+        enable = true;
+        settings = {
+            user = {
+                email = "minhthinle69@gmail.com";
+                name = "T0ast";
+            };
+        };
     };
-  };
 
-  programs.starship = {
-    enable = true;
-    settings = {
-      add_newline = false;
-      aws.disabled = true;
-      gcloud.disabled = true;
-      line_break.disabled = true;
+    programs.starship = {
+        enable = true;
+        settings = {
+            add_newline = false;
+            aws.disabled = true;
+            gcloud.disabled = true;
+            line_break.disabled = true;
+        };
     };
-  };
 
-  services.kdeconnect.enable = true;
+    xdg.desktopEntries = {
+        zen-browser-uni = {
+            name = "Zen Browser (University profile)";
+            genericName = "Web browser";
+            exec = "zen-twilight -P University";
+            terminal = false;
+            categories = [
+                "Network"
+                "WebBrowser"
+            ];
+            mimeType = [
+                "text/html"
+                "text/xml"
+            ];
+        };
+    };
 
-  home.stateVersion = "25.11";
+    services.kdeconnect.enable = true;
+
+    home.stateVersion = "25.11";
 }
