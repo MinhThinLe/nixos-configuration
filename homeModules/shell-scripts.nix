c08b50fa3769b091339748601616ab743183fe67
T0ast <minhthinle69@gmail.com>
2026-02-21 15:25:23 +0700

    Fixed nightlight and added note

--- homeModules/shell-scripts.nix
+++ homeModules/shell-scripts.nix
@@ -11,12 +11,11 @@
 
         (writeShellScriptBin "nightlight" ''
             ${pkgs.wlsunset}/bin/wlsunset \
-                -t 3500 \ # Color temperature
-                -s 19:30 \ # Start time
-                -S 6:30 \ # End time
+                -t 3500 \
+                -s 19:30 \
+                -S 6:30 \
         '')
 
-        wl-mirror
         (writeShellScriptBin "present" ''
             if  [ -e /run/user/1000/pipectl.1000.wl-present.pipe ]; then
                 rm /run/user/1000/pipectl.1000.wl-present.pipe
@@ -24,6 +23,13 @@
 
             ${pkgs.wl-mirror}/bin/wl-present mirror eDP-1 --fullscreen-output HDMI-A-1 --fullscreen
         '')
+
+        (writeShellScriptBin "note" ''
+            pushd /home/t0ast/stuff/notes
+            ${pkgs.neovim}/bin/nvim index.md
+            popd
+        '')
+
     ];
 
     home.file.".zprofile".text = ''
