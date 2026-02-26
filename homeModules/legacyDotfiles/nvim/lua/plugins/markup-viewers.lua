ac7a59f34874e5edf88a76480b994bdceabad8d8
T0ast <minhthinle69@gmail.com>
2026-02-26 16:37:55 +0700

    Fixed typst-preview

--- homeModules/legacyDotfiles/nvim/lua/plugins/markup-viewers.lua
+++ homeModules/legacyDotfiles/nvim/lua/plugins/markup-viewers.lua
@@ -4,7 +4,11 @@ return {
         ft = 'typst',
         version = '1.*',
         opts = {
-            open_cmd = "zen-beta --new-window %s"
+            open_cmd = "zen-beta --new-window %s",
+
+            dependencies_bin = {
+                ['tinymist'] = "tinymist",
+            },
         }
     },
 }
