444bd195b5b9a661dcd93136c7f4e39ae9ebb080
T0ast <minhthinle69@gmail.com>
2026-03-11 20:43:17 +0700

    Removed redundant spaces in powermenu

--- homeModules/legacyDotfiles/rofi/applets/bin/powermenu.sh
+++ homeModules/legacyDotfiles/rofi/applets/bin/powermenu.sh
@@ -11,7 +11,7 @@ theme="$type/$style"
 
 # Theme Elements
 prompt="`hostname`"
-mesg="Uptime: `uptime | sed 's/^.*up\s*//' | sed 's/,.*//g' | sed 's/:/ hours and /'` minutes"
+mesg="Uptime: `uptime | sed 's/^.*up\s*//' | sed 's/,.*//g' | sed 's/:/ hours /' | tr -s '[:space:]'` minutes"
 
 if [[ ( "$theme" == *'type-1'* ) || ( "$theme" == *'type-3'* ) || ( "$theme" == *'type-5'* ) ]]; then
 	list_col='1'
